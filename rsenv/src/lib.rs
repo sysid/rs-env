a4d934d61f1bc8efa05aa218f3e27fcd4374fb2a
sysid <sysid@gmx.de>
2023-08-31 21:34:44 +0200

    feat: nom based extraction of parents

--- rsenv/src/lib.rs
+++ rsenv/src/lib.rs
@@ -7,6 +7,11 @@ use anyhow::{Context, Result};
 use log::{debug, info};
 use std::{env, fs};
 use camino::{Utf8Path, Utf8PathBuf};
+use nom::bytes::complete::{tag, take_while};
+use nom::error::{dbg_dmp, Error, ParseError};
+use nom::{AsBytes, IResult, Parser};
+use nom::character::complete::multispace0;
+use nom::sequence::delimited;
 use pathdiff::diff_utf8_paths;
 use stdext::function_name;
 
@@ -286,6 +291,40 @@ pub fn link_all(nodes: &[String]) {
     }
 }
 
+// Parser to skip whitespace
+#[allow(dead_code)]
+fn space(input: &str) -> IResult<&str, &str> {
+    take_while(|c: char| c.is_whitespace())(input)
+}
+
+/// A combinator that takes a parser `inner` and produces a parser that also consumes both leading and
+/// trailing whitespace, returning the output of `inner`.
+#[allow(dead_code)]
+fn ws<'a, F, O, E: ParseError<&'a str>>(inner: F) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+  where
+  F: Parser<&'a str, O, E>,
+{
+  delimited(
+    multispace0,
+    inner,
+    multispace0
+  )
+}
+
+// Parser to extract the path after `# rsenv:`
+#[allow(dead_code)]
+fn extract_path(input: &str) -> IResult<&str, &str> {
+    dlog!("input: {:?}", input);
+    // dbg_dmp(tag::<&str, &[u8], Error<_>>("# rsenv:"),"xxx")(input.as_bytes());
+
+    let (input, _) = multispace0(input)?; // Match optional whitespace or newlines
+    let (input, _) = tag("# rsenv:")(input)?;
+    dlog!("input: {:?}", input);
+    // let (input, _) = space(input)?;
+    // dlog!("input: {:?}", input);
+    ws(take_while(|c: char| !c.is_whitespace()))(input)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -297,6 +336,22 @@ mod tests {
             .filter_level(log::LevelFilter::max())
             .is_test(true)
             .try_init();
+    }
+
+    #[test]
+    fn test_extract_path() {
+        let content = r#"
+# rsenv: level1.env
+
+# Level2 overwrite
+export VAR_4=var_42
+export VAR_5=var_52
+"#;
+
+        match extract_path(content) {
+            Ok((_, path)) => println!("Extracted path: {}", path),
+            Err(e) => println!("Error: {:?}", e),
+        }
     }
 
     #[test]
